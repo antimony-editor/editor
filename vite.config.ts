57dd354f05acac2206985292166ec0032ac30f47
Gen1xLol <gen1x.01.cool@gmail.com>
2026-06-01 21:03:56 -0300

    change some vite fuckery + hide the display thing which was a placeholder i forgot to remove

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,23 @@ export default defineConfig({
 	plugins: [react()],
 	base: process.env.NODE_ENV === 'production'
 		? '/editor/'
-		: '/'
+		: '/',
+	build: {
+		rollupOptions: {
+			output: {
+				codeSplitting: true,
+				manualChunks: (id) => {
+					if (id.includes('node_modules/blockly')) {
+						return 'blockly';
+					}
+					if (id.includes('node_modules/konva')) {
+						return 'konva';
+					}
+					if (id.includes('node_modules')) {
+						return 'vendor';
+					}
+				}
+			}
+		}
+	}
 });
