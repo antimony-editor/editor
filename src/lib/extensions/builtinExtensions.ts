1bd3dc3ab861b3e0e4cbfbf0bc15ed0f4342e8d5
dotman <111914552+aefarkalibastafinfein@users.noreply.github.com>
2026-06-27 08:07:12 +0100

    lazy ass commit

--- src/lib/extensions/builtinExtensions.ts
+++ src/lib/extensions/builtinExtensions.ts
@@ -2,24 +2,26 @@
 // built in extensions should be trusted
 
 export type ExtensionItem = {
-    name:string,
-    img?:string,
-    desc:string,
-    creator:string,
-    jsFile:string,
+    name: string,
+    img?: string,
+    desc: string,
+    creator: string,
+    jsFile: string,
 }
 
-export const extensions:ExtensionItem[] = [
+export const extensions: ExtensionItem[] = [
     {
         name: "Camera",
         desc: "2D Camera system for scrolling and zooming.",
         creator: "Antimony Team",
-        jsFile: "camera.js"
+        jsFile: "camera.js",
+        img: "camera.svg"
     },
     {
         name: "Sets",
         desc: "Set data structure blocks for storing unique values.",
         creator: "Antimony Team",
-        jsFile: "sets.js"
+        jsFile: "sets.js",
+        img: "sets.svg"
     }
 ]
