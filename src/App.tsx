9775e231913e5917a66678eab5ac1978240ef512
lordcat <171042544+LordCat0@users.noreply.github.com>
2026-06-30 20:00:54 -0400

    Why is this still here

--- src/App.tsx
+++ src/App.tsx
@@ -355,11 +355,6 @@ export default function App() {
           )}
 
           {showExtMenu && (
-            /*
-          <div style={{position: "absolute", top: 0, left: 0,color: "red", background:"green", width: "100%", height:"100%",zIndex: 10000}}>
-            SHOW EXT MENU HERE
-          </div>
-          */
             <ExtensionMenu showMenu={setShowExtMenu} />
           )}
         </ProjectSettingsContext.Provider>
